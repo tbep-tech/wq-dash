576ed8a47e9230c2ddf3b472c917e486a7cec236
fawda123 <mbafs2012@gmail.com>
2020-01-26 11:57:44 -0500

    plotlyfied threshold plots

--- R/funcs.R
+++ R/funcs.R
@@ -1,3 +1,4 @@
+# image nonsense because renderplot doesn't create correct font on shiny server
 img_fun <- function(rct, session, imgnm){
   
   width  <- session$clientData[[paste0('output_', imgnm, '_width')]]
@@ -20,4 +21,27 @@ img_fun <- function(rct, session, imgnm){
        height = height,
        alt = "")
 
+}
+
+# plotly function for threshold plots
+thrplotly <- function(epcdata, bay_segment, maxyr, family, themein){
+  
+  p1 <- show_thrplot(epcdata, bay_segment = bay_segment, thr = "chla", yrrng = c(1975, maxyr), family = family, txtlab = F, labelexp = F) + 
+    ggtitle(NULL) +
+    themein
+  p2 <- show_thrplot(epcdata, bay_segment = bay_segment, thr = "la", yrrng = c(1975, maxyr), family = family, txtlab = F, labelexp = F) + 
+    ggtitle(NULL) +
+    themein
+  
+  p1 <- ggplotly(p1)
+  p2 <- ggplotly(p2)
+  p2$x$data[[1]]$showlegend <- FALSE
+  p2$x$data[[2]]$showlegend <- FALSE
+  p2$x$data[[3]]$showlegend <- FALSE
+  p2$x$data[[4]]$showlegend <- FALSE
+  
+  out <- subplot(p1, p2, nrows = 2, heights = c(0.5, 0.5), shareX = T, titleY = TRUE)
+  
+  return(out)
+  
 }
