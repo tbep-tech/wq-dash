39a094f82c1efdd7ad403ca59bc7d7549fc270d3
fawda123 <mbafs2012@gmail.com>
2020-01-28 13:40:28 -0500

    algae data added, barplot counts added to time series tab

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
 # import and save epcdata -------------------------------------------------
 
 # # local file path
@@ -13,3 +17,35 @@ xlsx <- 'T:/03_BOARDS_COMMITTEES/05_TBNMC/BAY_TARGETS/2019_update/data-raw/epchc
 epcdata <- read_importwq(xlsx, download_latest_epchc = F, tryurl = T, connecttimeout = 20)
 
 save(epcdata, file = 'data/epcdata.RData', compress = 'xz')
+
+# algae data --------------------------------------------------------------
+
+data(epcdata)
+locs <- epcdata %>% 
+  select(epchc_station, Longitude, Latitude) %>% 
+  unique
+
+algraw <- read_excel('C:/users/Marcus.SCCWRP2K/Desktop/PlanktonDataList_ThroughCurrentReportMonth.xlsx')
+
+algdat <- algraw %>% 
+  select(epchc_station = StationNumber, Date = SampleTime, phylum = PHYLUM, name = NAME, count = COUNT, units = Units) %>% 
+  filter(epchc_station %in% locs$epchc_station) %>% 
+  mutate(
+    Date = as.Date(Date),
+    # qrt = quarter(Date, with_year = TRUE),
+    name = case_when(
+      name %in% c('Pyrodinium bahamense', 'Karenia brevis', 'Tripos hircus', 'Pseudo-nitzschia sp.', 'Pseudo-nitzschia pungens') ~ name, 
+      phylum %in% c('Bacillariophyta', 'Cyanobacteria') ~ phylum, 
+      !is.na(phylum) ~ 'other', 
+      T ~ NA_character_
+    )
+  ) %>% 
+  filter(!is.na(name)) %>% 
+  group_by(epchc_station, Date, name, units) %>% 
+  summarise(count = sum(count, na.rm = T)) %>% 
+  ungroup %>% 
+  mutate(
+    yrqrt = floor_date(Date, unit = 'quarter')
+  ) 
+
+save(algdat, file = here::here('data', 'algdat.RData'), compress = 'xz')
--- R/funcs.R
+++ R/funcs.R
@@ -1,3 +1,10 @@
+# algae color palette
+pal_alg <- colorFactor(
+  palette = RColorBrewer::brewer.pal(8,  'Dark2'),#c('#004F7E', '#00806E', '#427355', '#5C4A42', '#958984'),
+  na.color = 'yellow',
+  levels = c('Bacillariophyta', 'Cyanobacteria', 'Karenia brevis', 'Pseudo-nitzschia pungens', 'Pseudo-nitzschia sp.', 'Pyrodinium bahamense', 'Tripos hircus', 'other')
+  )
+
 # image nonsense because renderplot doesn't create correct font on shiny server
 img_fun <- function(rct, session, imgnm){
   
