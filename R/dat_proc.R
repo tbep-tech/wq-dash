3fc6b8fc00f0cb0205330fe796cd8aa02d0c6c18
fawda123 <mbafs2012@gmail.com>
2020-03-10 11:11:09 -0400

    nonreactive plots saved as data objects

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,41 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(tbeptools)
+library(plotly)
+library(extrafont)
+library(leaflet)
+
+source('R/funcs.R')
+
+loadfonts(device = 'pdf', quiet = T)
+if(Sys.info()[1] == 'Windows')
+  loadfonts(device = 'win', quiet = T)
+
+maxyr <- 2019
+fml <- "Lato Light"
+
+# minor theme tweaks
+pthm <- theme(
+  axis.text.x = element_text(size = 11, angle = 45, hjust = 1),
+  legend.text = element_text(size = 12), 
+  axis.title.y = element_text(size = 12),
+  text = element_text(fml), 
+  legend.position = 'top',
+  # panel.grid.minor=element_blank(),
+  # panel.grid.major=element_blank(),
+  panel.background = element_rect(fill = '#ECECEC')
+) 
+
+# plotly secondary axix
+ax <- list(
+  tickfont = list(size=14),
+  overlaying = "x",
+  nticks = 4,
+  side = "top"
+)
+seg <- c('OTB', 'HB', 'MTB', 'LTB') 
+nms <- factor(seg, levels = seg)
 
 # import and save epcdata -------------------------------------------------
 
@@ -51,3 +86,51 @@ algdat <- algraw %>%
   ) 
 
 save(algdat, file = here::here('data', 'algdat.RData'), compress = 'xz')
+
+# graphics for dash -------------------------------------------------------
+
+# attainment matrix
+plo <- show_matrix(epcdata, yrrng = c(1975, maxyr), family = fml, txtsz = NULL) + 
+  theme(
+    axis.text = element_text(size = 12),
+    text = element_text(family = fml),
+    axis.text.x = element_blank(), 
+    axis.ticks.x = element_blank()
+  )
+attmat <- ggplotly(plo, tooltip = 'Action') %>%
+  add_bars(x = nms,y = c(1, 1, 1, 1), xaxis = "x2", inherit = F) %>%
+  layout(xaxis2 = ax)
+
+# chl matrix
+plo <- show_wqmatrix(epcdata, param = 'chla', yrrng = c(1975, maxyr), family = fml, txtsz = NULL) + 
+  theme(
+    axis.text = element_text(size = 12), 
+    text = element_text(family = fml),
+    axis.text.x = element_blank(), 
+    axis.ticks.x = element_blank()
+  ) 
+chlmat <- ggplotly(plo, tooltip = 'Result') %>%
+  add_bars(x = nms,y = c(1, 1, 1, 1), xaxis = "x2", inherit = F) %>%
+  layout(xaxis2 = ax)
+  
+# la matrix
+plo <- show_wqmatrix(epcdata, param = 'la', yrrng = c(1975, maxyr), family = fml, txtsz = NULL) + 
+  theme(
+    axis.text = element_text(size = 12), 
+    text = element_text(family = fml),
+    axis.text.x = element_blank(), 
+    axis.ticks.x = element_blank()
+  ) 
+lamat <- ggplotly(plo, tooltip = 'Result') %>%
+  add_bars(x = nms,y = c(1, 1, 1, 1), xaxis = "x2", inherit = F) %>%
+  layout(xaxis2 = ax)
+
+# threshold plots
+thrplototb <- thrplotly(epcdata, 'OTB', maxyr, fml, pthm)
+thrplothb <- thrplotly(epcdata, 'HB', maxyr, fml, pthm)
+thrplotmtb <- thrplotly(epcdata, 'MTB', maxyr, fml, pthm)
+thrplotltb <- thrplotly(epcdata, 'LTB', maxyr, fml, pthm)
+
+save(thrplototb, thrplothb, thrplotmtb, thrplotltb, attmat, chlmat, lamat, file = 'data/plotlys.RData', compress = 'xz')
+
+
