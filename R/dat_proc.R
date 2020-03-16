a4080b82acb470e7ee0dc46631a3c147bd497dca
fawda123 <mbafs2012@gmail.com>
2020-03-16 10:40:14 -0400

    alg data processing now uses tbeptools function

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -55,35 +55,12 @@ nms <- factor(seg, levels = seg)
 # 
 # # algae data --------------------------------------------------------------
 # 
-# data(epcdata)
-# locs <- epcdata %>% 
-#   select(epchc_station, Longitude, Latitude) %>% 
-#   unique
+# # file path
+# xlsx <- 'C:/Users/Marcus/Desktop/phyto_data.xlsx'
 # 
-# algraw <- read_excel('C:/users/Marcus.SCCWRP2K/Desktop/PlanktonDataList_ThroughCurrentReportMonth.xlsx')
-# 
-# algdat <- algraw %>% 
-#   select(epchc_station = StationNumber, Date = SampleTime, phylum = PHYLUM, name = NAME, count = COUNT, units = Units) %>% 
-#   filter(epchc_station %in% locs$epchc_station) %>% 
-#   mutate(
-#     Date = as.Date(Date),
-#     # qrt = quarter(Date, with_year = TRUE),
-#     name = case_when(
-#       name %in% c('Pyrodinium bahamense', 'Karenia brevis', 'Tripos hircus', 'Pseudo-nitzschia sp.', 'Pseudo-nitzschia pungens') ~ name, 
-#       phylum %in% c('Bacillariophyta', 'Cyanobacteria') ~ phylum, 
-#       !is.na(phylum) ~ 'other', 
-#       T ~ NA_character_
-#     )
-#   ) %>% 
-#   filter(!is.na(name)) %>% 
-#   group_by(epchc_station, Date, name, units) %>% 
-#   summarise(count = sum(count, na.rm = T)) %>% 
-#   ungroup %>% 
-#   mutate(
-#     yrqrt = floor_date(Date, unit = 'quarter'),
-#     yr = year(Date), 
-#     mo = month(Date, label = T)
-#   ) 
+# # load and assign to object
+# algdat <- read_importphyto(xlsx, download_latest_epchc = T, tryurl = T) %>% 
+#   filter(yr < 2020)
 # 
 # save(algdat, file = here::here('data', 'algdat.RData'), compress = 'xz')
 
